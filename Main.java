fb6060328a6e0e4e72ede3f9481f0755281a0f0e
Omer Bar Ness <omer.bn@algo.team>
2017-10-28 20:12:29 +0300

    added tests for all methods

--- Main.java
+++ Main.java
@@ -4,13 +4,15 @@
 public class Main {
 
     static  int testsFailed = 0;
+    static int testNumber = 1;
     static StringBuilder sb = new StringBuilder();
 
-    public static void test(int testNumber,String expected, String result){
+    public static void test(String expected, String result){
         if(expected.equals(result)){
+            testNumber++;
         }
         else{
-            sb.append(("test number " + testNumber + " failed\n"));
+            sb.append(("test number " + testNumber++ + " failed\n"));
             testsFailed++;
         }
     }
@@ -21,46 +23,51 @@ public class Main {
         Rational r3 = new Rational(2,5);
         Rational r4 = new Rational(10,49);
 
-        int testNumber = 1;
+
 
         //test constructor & to string
-        test(testNumber++, "1/2", r1.toString() );
-        test(testNumber++, "2/4", r2.toString() );
-        test(testNumber++, "2/5", r3.toString() );
-        test(testNumber++, "10/49", r4.toString() );
+        test( "1/2", r1.toString() );
+        test( "2/4", r2.toString() );
+        test( "2/5", r3.toString() );
+        test( "10/49", r4.toString() );
 
         //test reduce
-        test(testNumber++, "1/2", r2.reduce().toString());
-        test(testNumber++, "1/2", r1.reduce().toString());
-        test(testNumber++, "2/5", r3.reduce().toString());
-        test(testNumber++, "10/49", r4.reduce().toString());
+        test( "1/2", r2.reduce().toString());
+        test( "1/2", r1.reduce().toString());
+        test( "2/5", r3.reduce().toString());
+        test( "10/49", r4.reduce().toString());
 
 
         //test plus
 
-        test(testNumber++, "1/1", r2.plus(r1).reduce().toString());
-        test(testNumber++, "9/10", r2.plus(r3).reduce().toString());
-        test(testNumber++, "9/10", r3.plus(r2).reduce().toString());
-        test(testNumber++, "69/98", r4.plus(r1).reduce().toString());
+        test("1/1", r2.plus(r1).reduce().toString());
+        test("9/10", r2.plus(r3).reduce().toString());
+        test("9/10", r3.plus(r2).reduce().toString());
+        test("69/98", r4.plus(r1).reduce().toString());
 
         //test minus
-        test(testNumber++, "0/1", r1.minus(r1).reduce().toString());
-        test(testNumber++, "0/1", r1.minus(r2).reduce().toString());
-        test(testNumber++, "-1/10", r3.minus(r2).reduce().toString());
-        test(testNumber++, "0/1", r4.minus(r3).reduce().toString());
+        test("0/1", r1.minus(r1).reduce().toString());
+        test("0/1", r1.minus(r2).reduce().toString());
+        test("-1/10", r3.minus(r2).reduce().toString());
+        test("-48/245", r4.minus(r3).reduce().toString());
 
-        //test multiply
-
+        test("1/4", r1.multiply(r1).reduce().toString());
+        test("1/4", r1.multiply(r2).reduce().toString());
+        test("1/5", r3.multiply(r2).reduce().toString());
+        test("1/5", r2.multiply(r3).reduce().toString());
+        test("4/49", r4.multiply(r3).reduce().toString());
 
         //test equals
+        test("true", String.valueOf(r1.equals(r1)));
+        test("true", String.valueOf(r1.equals(r2)));
+        test("false", String.valueOf(r1.equals(r4)));
+        test("false", String.valueOf(r1.equals(r3)));
 
         //test greaterThan
-
-        //test greaterThan
-
-        //test greaterThan
-
-        //test greaterThan
+        test("false", String.valueOf(r1.greaterThan(r1)));
+        test("false", String.valueOf(r1.greaterThan(r2)));
+        test("true", String.valueOf(r1.greaterThan(r3)));
+        test("true", String.valueOf(r1.greaterThan(r4)));
 
 
 
